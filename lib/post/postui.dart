889f8aa19267784ac8e473bad21ca71c645b9a89
Shovon Hossain <shovonsemail@gmail.com>
2018-04-09 20:50:22 -0400

    changed column to listview

--- lib/post/postui.dart
+++ lib/post/postui.dart
@@ -4,12 +4,16 @@ import 'body.dart';
 import 'header.dart';
 
 Widget postUI(Content post){
-  return new Card(
-    child: new Column(
-      children: <Widget>[
-        postHeaderUI(post),
-        postBodyUI(post),
-      ],
-    ),
+  return new ListView(
+    children: <Widget>[
+      new Card(
+        child: new Column(
+          children: <Widget>[
+            postHeaderUI(post),
+            postBodyUI(post),
+          ],
+        ),
+      )
+    ],
   );
 }
